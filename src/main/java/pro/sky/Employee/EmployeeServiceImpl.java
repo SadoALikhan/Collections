bba5f23ba8406d4c62ba8ce9e36e31555bba6091
SadoALikhan <khan.ali.99.sado@mail.ru>
2024-06-07 23:33:15 +0300

    Homework

---
+++ src/main/java/pro/sky/Employee/EmployeeServiceImpl.java
@@ -0,0 +1,10 @@
+package pro.sky.Employee;
+
+import java.util.*;
+
+public class EmployeeServiceImpl implements EmployeeService {
+    List<Employee> employees = new ArrayList<>(List.of(
+            ("Иванов", "Иван"),
+
+    ));
+}
