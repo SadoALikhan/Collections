31f5680a575adbc447c8d593f58d9bc2576b9207
SadoALikhan <khan.ali.99.sado@mail.ru>
2024-06-08 18:58:12 +0300

    Homework

--- src/main/java/pro/sky/Employee/EmployeeController.java
+++ src/main/java/pro/sky/Employee/EmployeeController.java
@@ -1,12 +1,37 @@
 package pro.sky.Employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pro.sky.Employee.Exception.EmployeeAlreadyAddedException;
+import pro.sky.Employee.Exception.EmployeeStorageIsFullException;
+import pro.sky.Employee.Exception.WrongFormatException;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+
+    @GetMapping(path = "/add")
+    public String add(@RequestParam(value = "lastName", required = false) String lastName,
+                      @RequestParam(value = "firstName", required = false) String firstName) {
+        employeeService.addEmployee(lastName, firstName);
+        try {
+            employeeService.addEmployee(lastName, firstName);
+        } catch (NullPointerException e) {
+            System.out.println("Одно из полей не заполнено(проверьте поля фамилия/имя).");
+        } catch (WrongFormatException e) {
+            System.out.println("Использован неподходящий символ(проверьте поля фамилия/имя).");
+        } catch (EmployeeStorageIsFullException e) {
+            System.out.println("Превышен лимит количества сотрудников в фирме.");
+        } catch (EmployeeAlreadyAddedException e) {
+            System.out.println("Сотрудник уже числится в списке.");
+        }
+        return "lastName: " + lastName.cap + ", firstName: " + firstName.toUpperCase();
     }
 }
--- src/main/java/pro/sky/Employee/EmployeeService.java
+++ src/main/java/pro/sky/Employee/EmployeeService.java
@@ -1,9 +1,108 @@
 package pro.sky.Employee;
 
-public interface EmployeeService {
-    void addEmployee(String lastName, String firsName);
+import org.springframework.stereotype.Service;
+import pro.sky.Employee.Exception.EmployeeAlreadyAddedException;
+import pro.sky.Employee.Exception.EmployeeNotFoundException;
+import pro.sky.Employee.Exception.EmployeeStorageIsFullException;
+import pro.sky.Employee.Exception.WrongFormatException;
 
-    void removeEmployee(String lastName, String firsName);
+import java.util.*;
 
-    Employee findEmployee(String lastName, String firsName);
+@Service
+public class EmployeeService {
+
+    public String simbols = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
+    private final int maxEmployee = 99;
+    private List<Employee> employees = new ArrayList<>(List.of(
+            new Employee("Иванов", "Иван"),
+            new Employee("Леонов", "Никита"),
+            new Employee("Степанов", "Алексей"),
+            new Employee("Семёнов", "Ярослав"),
+            new Employee("Максимов", "Николай"),
+            new Employee("Степанов", "Ермак"),
+            new Employee("Морозов", "Василий"),
+            new Employee("Михайлов", "Артур"),
+            new Employee("Кешелев", "Константин"),
+            new Employee("Тихонов", "Вадим")
+    ));
+
+    public void addEmployee(String lastName, String firsName) {
+        Employee employee = new Employee(lastName, firsName);
+        char[] a = lastName.toCharArray();
+        char[] b = firsName.toCharArray();
+        if (employee.getLastName() == null || employee.getFirstName() == null) {
+            throw new NullPointerException();
+        }
+        for (char d : a) {
+            if (!simbols.contains(String.valueOf(d))) {
+                throw new WrongFormatException();
+            }
+        }
+        for (char d : b) {
+            if (!simbols.contains(String.valueOf(d))) {
+                throw new WrongFormatException();
+            }
+        }
+        if (employees.size() == maxEmployee) {
+            throw new EmployeeStorageIsFullException();
+        } else if (employees.contains(employee)) {
+            throw new EmployeeAlreadyAddedException();
+        } else {
+            employee.getLastName().toUpperCase(a[0]);
+            employee.getFirstName().toUpperCase();
+            employees.add(employee);
+        }
+    }
+
+    public void removeEmployee(String lastName, String firsName) {
+        Employee employee = new Employee(lastName, firsName);
+        char[] a = lastName.toCharArray();
+        char[] b = firsName.toCharArray();
+        if (employee.getLastName() == null || employee.getFirstName() == null) {
+            throw new NullPointerException();
+        }
+        for (char d : a) {
+            if (!simbols.contains(String.valueOf(d))) {
+                throw new WrongFormatException();
+            }
+        }
+        for (char d : b) {
+            if (!simbols.contains(String.valueOf(d))) {
+                throw new WrongFormatException();
+            }
+        }
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        } else {
+            employee.getLastName().toUpperCase();
+            employee.getFirstName().toUpperCase();
+            employees.remove(employee);
+        }
+    }
+
+    public Employee findEmployee(String lastName, String firsName) {
+        Employee employee = new Employee(lastName, firsName);
+        char[] a = lastName.toCharArray();
+        char[] b = firsName.toCharArray();
+        if (employee.getLastName() == null || employee.getFirstName() == null) {
+            throw new NullPointerException();
+        }
+        for (char d : a) {
+            if (!simbols.contains(String.valueOf(d))) {
+                throw new WrongFormatException();
+            }
+        }
+        for (char d : b) {
+            if (!simbols.contains(String.valueOf(d))) {
+                throw new WrongFormatException();
+            }
+        }
+        if (!employees.contains(employee)) {
+            throw new EmployeeNotFoundException();
+        }
+        employee.getLastName().toUpperCase();
+        employee.getFirstName().toUpperCase();
+        return employee;
+    }
 }
